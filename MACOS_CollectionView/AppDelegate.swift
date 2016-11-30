f99d2a93a8ee8631aac7e72e64b7d6d6fd737201
WalkWithWind <sycf_ios_13579@sycf-iosdeiMac.local>
2016-11-30 11:23:08 +0800

    Initial Commit

---
+++ MACOS_CollectionView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MACOS_CollectionView
+//
+//  Created by sycf_ios on 2016/11/30.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MACOS_CollectionView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MACOS_CollectionView
+//
+//  Created by sycf_ios on 2016/11/30.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
